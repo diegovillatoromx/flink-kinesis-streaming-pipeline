65ee8841df58a0b1f7ecf22f3fb685677f75b324
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2023-10-25 15:44:23 -0600

    Update kinesis_stream_stack.py

--- Data_Pipeline/raw_layer/stream_1/kinesis_stream_stack.py
+++ Data_Pipeline/raw_layer/stream_1/kinesis_stream_stack.py
@@ -2,17 +2,16 @@ from aws_cdk import Stack, aws_kinesis as kinesis, Duration
 from constructs import Construct
 from decouple import config
 
-INTRADAY_STREAM_NAME = config("INTRADAY_STREAM_NAME")
-
+FIRST_STREAM_NAME = config("FIRST_STREAM_NAME")
 
 class KinesisStreamStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        intraday_stream = kinesis.Stream(
+        stream_name = kinesis.Stream(
             self,
-            "IntradayKinesisStream",
-            stream_name=INTRADAY_STREAM_NAME,
+            "KinesisStream",
+            stream_name=FIRST_STREAM_NAME,
             shard_count=1,
             retention_period=Duration.hours(24),
         )
