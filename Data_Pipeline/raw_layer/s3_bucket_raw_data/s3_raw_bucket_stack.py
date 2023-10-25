2b7e0932b7012ac1be9378fd3c013ae345e096e2
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2023-10-25 15:43:14 -0600

    Update s3_raw_bucket_stack.py

--- Data_Pipeline/raw_layer/s3_bucket_raw_data/s3_raw_bucket_stack.py
+++ Data_Pipeline/raw_layer/s3_bucket_raw_data/s3_raw_bucket_stack.py
@@ -1,15 +1,11 @@
-from aws_cdk import Stack, aws_s3 as s3
 
 from constructs import Construct
 from decouple import config
 
-# Env. variables; i.e. can be OS variables in Lambda
-# This is the name of the S3 bucket that will store the raw data
-RAW_DATA_BUCKET_NAME = config("RAW_DATA_BUCKET_NAME")
+DATASET_BUCKET_NAME = config("DATASET_BUCKET_NAME")
 
-
-class RawDataBucketStack(Stack):
+class S3BucketStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        s3_bucket = s3.Bucket(self, id="RawDataBucket", bucket_name=RAW_DATA_BUCKET_NAME)
+        s3_bucket = s3.Bucket(self, id="DATASET_BUCKET_NAME", bucket_name=DATASET_BUCKET_NAME)
